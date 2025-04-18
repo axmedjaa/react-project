481d75a6ac3c11a6bc28dc36519db60f7a2b9bc7
axmedjaa <axmedja44@gmail.com>
2025-04-18 23:46:26 +0300

    updated main with adding pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-<<<<<<< HEAD
-import App from './App.jsx'
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-=======
 import { RouterProvider } from 'react-router'
 import router from './router'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
    <RouterProvider router={router}/>
->>>>>>> pages
   </StrictMode>,
 )
